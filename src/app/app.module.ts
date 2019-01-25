8735ef6821bbe96fae052f2a6e05987d1125f693
elifdogali <elif.dogali@ozu.edu.tr>
2019-01-25 16:23:09 +0300

    Add menu items and their styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule }    from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent, MenuItemComponent } from './components';
@@ -14,6 +15,7 @@ import { SpinnerModule } from './+lib';
   ],
   imports: [
     BrowserModule,
+    FormsModule,  
     HttpClientModule,
     SpinnerModule
   ],
